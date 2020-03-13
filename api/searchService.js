d20db4f99d3bad3aa73061c6146e86a5f42b4a61
OTree <kevin10410@gmail.com>
2020-03-13 16:21:08 +0800

    refactor(searchService): add fetchShowDetailById method

--- api/searchService.js
+++ api/searchService.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 
 export const fetchShowsByName = name => axios
   .get(`https://api.tvmaze.com/search/shows?q=${name}`);
+
+export const fetchShowDetailById = id => axios
+  .get(`https://api.tvmaze.com/shows/${id}`);
