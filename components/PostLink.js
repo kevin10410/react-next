e3028c9df0305dba5b198d3a3c4cb90c5ece273c
OTree <kevin10410@gmail.com>
2020-03-13 14:12:37 +0800

    refactor(PostLink): create PostLink component

---
+++ components/PostLink.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const PostLink = props => {
+
+  return (
+    <li>
+      <Link
+        href={`/post?title=${props.title}`}
+      >
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
+};
+
+export default PostLink;
