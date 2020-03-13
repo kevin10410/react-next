bdd67a04096ca1d55223f75c0b18fcffe2d289ea
OTree <kevin10410@gmail.com>
2020-03-13 15:53:36 +0800

    refactor(pages): add Index.getInitialProps to fetch data when build

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
+import {
+  fetchShowsByName,
+} from '../api/searchService';
+
 import Layout from '../components/UI/Layout';
 import PostLink from '../components/PostLink';
 
-const Index = () => {
+const Index = props => {
 
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
+        {
+          props.shows &&
+            props.shows.map(show => (
+              <PostLink
+                key = { show.id }
+                id = { show.id }
+                name = { show.name }
+              />
+          ))
+        }
       </ul>
     </Layout>
   );
+};
+
+Index.getInitialProps = async function () {
+  // Call an external API endpoint to get posts.
+  const data = await fetchShowsByName('batman')
+    .then(res => res.data);
+
+  return {
+    shows: data.map(entry => entry.show),
+  };
 };
 
 export default Index;
