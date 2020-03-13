59166425787cbbccd82b5240f0756131d7833936
OTree <kevin10410@gmail.com>
2020-03-13 16:22:05 +0800

    refactor([id]): add getInitialProps to fetch data by id query

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,15 +1,28 @@
-import { useRouter } from 'next/router';
+import {
+  fetchShowDetailById
+} from '../../api/searchService';
+
 import Layout from '../../components/UI/Layout';
 
 const Post = props => {
-  const router = useRouter();
 
   return (
     <Layout>
-      <h1>{ router.query.id }</h1>
-      <p>This is the blog post content.</p>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+      {props.show.image ? <img src={props.show.image.medium} /> : null}
     </Layout>
   );
+};
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const data = await fetchShowDetailById(id)
+    .then(res => res.data);
+
+  return {
+    show: data,
+  };
 };
 
 export default Post;
